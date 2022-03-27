319640c34930c41f55fd9ee0f276084b49b40808
obergad <amoallmoa@Gaming-Masterpeice.localdomain>
2022-03-27 01:32:42 -0700

    website upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import Home from "./components/Home.js";
+import YoutubeEmbed from "./components/YoutubeEmbed";
+import Header from "./components/Header.js";
+import About from "./components/About.js";
+import Footer from "./components/Footer.js";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>      
+      <Home/>
+      <About />
+      <Footer/>
+
     </div>
   );
 }
---
+++ src/starfield.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+import StarfieldAnimation from 'react-starfield-animation'
+
+class Starfield extends Component {
+  render () {
+    return (
+      <StarfieldAnimation className="Starfield"/>
+    )
+  }
+}
+export default Starfield
